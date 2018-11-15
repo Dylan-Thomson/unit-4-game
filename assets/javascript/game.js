b99da960d4f460822fa3e85898587f81312f30fc
Dylan Thomson <djthomson88@gmail.com>
2018-11-14 21:11:04 -0500

    Some cleanup and documentation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,3 @@
-/*
--Choose from 4 characters
--Move 3 enemies to a new div
--Choose an enemy to attack
--Selected enemy moves to defender area
--User hits attack button
-  -Player reduces enemy hitpoints by his attack power
-  -Check enemy HP. If it drops below zero, remove from defender area and let player pick new opponent. Pressing attack tells player to choose new opponent.
-    -If enemy dies, check enemies remaining. If there are no enemies remaining, display win message and restart button which allows player to play again.
-  -Enemy reduces player hitpoints by his counterattack power if he survives
-  -Check player HP. If it drops below zero, the game ends. Show restart button which allows player to pick a new character.
-  -Increase player attack power by base attack power 
-*/
-
 class Fighter {
     constructor(name, healthPoints, attackPower, counterAttackPower) {
         this.name = name;
@@ -34,7 +20,6 @@ Fighter.prototype.toString = function fighterToString() {
            "Counter Attack: " + this.counterAttackPower + "\n" +
            "******************************\n";
 }
-
 
 class Game {
     constructor(fighters) {
@@ -80,11 +65,6 @@ class Game {
             if(!this.enemyFighters.length) {
                 console.log("You won!");
             }
-            else {
-                //pick new fighter
-            }
-            //if there are enemies left, have player pick new enemy
-            //otherwise game ends
         }
         function logAttack(attacker, defender, damage) {
             console.log(
@@ -110,25 +90,28 @@ function init() {
     game = new Game(fighters);
 }
 
-    
 $(document).ready(function() {
     init();
 
     $(".fighter").on("click", function() {
         var fighter = game.fighters[$(this).attr("id")];
+        // Player selects fighter if none selected yet
         if(!game.playerFighter) {
             $(this).appendTo("#attacker-area");
             game.playerFighter = fighter;
+            // Add other fighters to enemyFighters array
             game.enemyFighters = Object.values(game.fighters).filter((fighter) => {
                 return fighter !== game.playerFighter;
             });
         }
+        // Player selects an enemy
         else if(!game.currentEnemy && fighter !== game.playerFighter) {
             $(this).appendTo("#defender-area");
             game.currentEnemy = fighter;
         }
     });
-        
+    
+    // Player clicks attack
     $("#attack-btn").on("click", function() {
         if(game.playerFighter && game.currentEnemy) {
             console.log(game.currentEnemy);
@@ -136,54 +119,3 @@ $(document).ready(function() {
         }
     });
 });
-
-// function attack() {
-//     // damage enemy
-//     currentEnemy.healthPoints -= playerFighter.attackPower;
-
-//     logAttack(playerFighter, currentEnemy, playerFighter.attackPower);
-    
-//     // enemy counterattacks if they survive
-//     if(currentEnemy.healthPoints > 0) {
-//         playerFighter.healthPoints -= currentEnemy.counterAttackPower;
-//         logAttack(currentEnemy, playerFighter, currentEnemy.counterAttackPower);
-
-//         // levelUp if player survives
-//         if(playerFighter.healthPoints > 0) {
-//             playerFighter.levelUp();
-//         }
-//         else {
-//             console.log("You died.");
-//             playerFighter = null;
-//         }
-//     }
-//     else {
-//         //remove enemy
-//         console.log("Enemy defeated!");
-//         enemyFighters = enemyFighters.filter(function(fighter) {
-//             return fighter !== currentEnemy;
-//         });
-//         currentEnemy = null;
-
-
-//         console.log(enemyFighters);
-//         //check enemies remaining
-//         if(!enemyFighters) {
-//             //game over
-//         }
-//         else {
-//             //pick new fighter
-//         }
-//         //if there are enemies left, have player pick new enemy
-//         //otherwise game ends
-//     }
-//     function logAttack(attacker, defender, damage) {
-//         console.log(
-//             attacker.name + " attacks " +
-//             defender.name + " for " +
-//             damage + " damage.\n" +
-//             defender.name + " has " +
-//             defender.healthPoints + " remaining!"
-//         );
-//     }
-// }
